6a014b780ae320913d1413246728a098c38e3af6
MyferIsADev <149029289+myferr@users.noreply.github.com>
2024-03-31 01:28:55 -0500

    📦

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,32 @@
-<script setup>
-import HelloWorld from './components/HelloWorld.vue'
-</script>
-
 <template>
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
+  <div id="bar">
+    <b>Spammer GUI</b><a href="https://github.com/myferr/spammer-gui">GitHub</a><a href="https://github.com/myferr/spammer-gui/releases">Releases</a><a href="https://github/myferr/spammer-gui/files/14814049/spammer-gui.zip">Download</a>
   </div>
-  <HelloWorld msg="Vite + Vue" />
+  <div id="main">
+    <h1>Spammer GUI</h1>    
+    <a href="https://github/myferr/spammer-gui/files/14814049/spammer-gui.zip"><button>Download</button></a>
+  </div>
+  <div id="about">
+    <h2>About</h2>
+    <span style="font-family:monospace;">
+      Spammer GUI is a python-based message sending macro with a graphical user interface <i>(GUI)</i>
+    </span>
+  </div>
+  <div id="credits">
+    <h2>Credits</h2>
+    <span style="font-family: monospace;">
+      Web development by Myfer
+      <br>App development by Myfer
+      <br><br>> https://github.com/myferr/
+    </span>
+  </div>
+  <div id="releases-page">
+    <h1>Releases</h1>
+    <span class="releases">
+      <a href="https://github.com/myferr/spammer-gui/releases/latest">LATEST VERISON</a>
+    </span>
+    <span class="releases">
+      <a href="https://github.com/myferr/spammer-gui/releases/tag/v1.0.0">spammer-gui -- v1.0.0[ALPHA]</a>
+    </span>
+  </div>
 </template>
-
-<style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
-</style>
